9ceaa2d6e7d11a2cfc32dca4ebbaf12cbd71407a
Woewal <d.woering@hotmail.com>
2024-01-26 16:36:38 +0000

    Adding scoring

--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour
+public class ScoreManager : Singleton<ScoreManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Score = MaxScore;
+    public const float MaxScore = 100;
+
+    public float DecreaseSpeed = 1;
+
+    public Action<float> OnChange;
+    public Action OnEmpty;
+    public bool isEmpty { get { return Score == 0; } }
+
+
+    private void Update()
     {
-        
+        if(!isEmpty)
+            Change(-Time.deltaTime * DecreaseSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Change(float amount)
     {
-        
+        var previous = Score;
+
+        Score += amount;
+
+        if(Score > MaxScore)
+        {
+            Score = MaxScore;
+        }
+
+        if(Score < 0)
+        {
+            Score = 0;
+            OnEmpty?.Invoke();
+        }
+
+        if (previous != Score)
+        {
+            OnChange?.Invoke(Score);
+        }
     }
 }
--- Assets/Script/Utility/Singleton.cs
+++ Assets/Script/Utility/Singleton.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    protected static T instance;
 
-    // Update is called once per frame
-    void Update()
+    //Returns the instance of this singleton.
+    public static T Instance
     {
-        
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+
+                if (instance == null)
+                {
+                    Debug.LogError("An instance of " + typeof(T) +
+                       " is needed in the scene, but there is none.");
+                }
+            }
+
+            return instance;
+        }
     }
 }
