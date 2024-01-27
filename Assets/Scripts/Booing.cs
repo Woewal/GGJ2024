76f15e299b00a637b9028d19fb0889f15a4fdcd2
Woewal <d.woering@hotmail.com>
2024-01-27 09:04:28 +0000

    added booing

---
+++ Assets/Scripts/Booing.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booing : MonoBehaviour
+{
+    ScoreManager ScoreManager;
+    [SerializeField] AudioSource BooSource;
+    [SerializeField] AnimationCurve Curve;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreManager = ScoreManager.Instance;
+
+        ScoreManager.OnChange += ChangeVolume;
+    }
+
+    void ChangeVolume (float amount) {
+        BooSource.volume = Curve.Evaluate(amount / ScoreManager.MaxScore);
+    }
+}
