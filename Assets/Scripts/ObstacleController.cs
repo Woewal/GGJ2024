35b109ce4c944b89ffa67998069e23fa2eb695b5
Woewal <d.woering@hotmail.com>
2024-01-27 17:42:27 +0100

    sound improvements & prefab improvements

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -25,7 +25,11 @@ public class ObstacleController : MonoBehaviour
         transform.DOMove(new Vector3(Random.Range(-5, 5),Random.Range(10, 30), Random.Range(10, 20)), animationLength);
         transform.DOScale(Vector3.zero, animationLength);
         AudioSource.PlayClipAtPoint(hitSound[Random.Range(0, hitSound.Count)], transform.position);
+        transform.DOPunchScale(Vector3.one * .3f, 0.1f, 0, 0);
         var Player = collider.gameObject.GetComponent<PlayerHit>();
-        if (Player) Player.Hit();
+        if (Player) {
+            Player.Hit();
+            ScoreManager.Instance.Score++;
+        } 
     }
 }
