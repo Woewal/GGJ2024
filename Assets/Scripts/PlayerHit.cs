673fca3fd09a23587a292835e28a7cbf00e9327f
Woewal <d.woering@hotmail.com>
2024-01-27 08:33:01 +0000

    -

---
+++ Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerHit : MonoBehaviour
+{
+    public Animator Animator;
+    [SerializeField] List<AudioClip> hitSounds;
+    [SerializeField] AudioClip laughSound;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Hit();
+        }
+    }
+
+    public void Hit()
+    {
+        var randomClip = hitSounds[Random.Range(0, hitSounds.Count)];
+        ScoreManager.Instance.Change(10);
+
+        Animator.transform.DOPunchScale(Vector3.one * .3f, 0.1f, 0, 0);
+
+        GameObject audioObject = new GameObject("DynamicAudioSource");
+        AudioSource audioSource = audioObject.AddComponent<AudioSource>();
+        audioSource.clip = randomClip;
+
+        audioSource.pitch = Random.Range(0.9f, 1.2f);
+        audioSource.transform.position = transform.position;
+        audioSource.Play();
+        Destroy(audioObject, audioSource.clip.length);
+
+        GameObject  laughObject = new GameObject("DynamicAudioSource");
+        AudioSource laughSource = laughObject.AddComponent<AudioSource>();
+        laughSource.clip = laughSound;
+
+        laughSource.pitch = Random.Range(0.9f, 1.2f);
+        laughSource.transform.position = transform.position;
+        laughSource.Play();
+        Destroy(laughObject, laughSource.clip.length);
+
+        this.Animator.SetTrigger("stumble");
+
+    }
+}
