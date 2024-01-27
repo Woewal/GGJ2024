d4ac704239c46bad8d13c7c85f0e83c4d456c457
Woewal <d.woering@hotmail.com>
2024-01-27 22:45:04 +0100

    Gold bars

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -7,6 +7,7 @@ using DG.Tweening;
 public class ObstacleController : MonoBehaviour
 {
     public List<AudioClip> hitSound;
+    public bool IsPositive;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +23,28 @@ public class ObstacleController : MonoBehaviour
     void OnTriggerEnter(Collider collider)
     {
         var animationLength = 2f;
-        transform.DOMove(new Vector3(Random.Range(-5, 5), Random.Range(10, 30), Random.Range(10, 20)), animationLength);
-        transform.DOScale(Vector3.zero, animationLength);
+        if(!IsPositive)
+        {
+            transform.DOMove(new Vector3(Random.Range(-5, 5), Random.Range(10, 30), Random.Range(10, 20)), animationLength);
+            transform.DOScale(Vector3.zero, animationLength);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         AudioSource.PlayClipAtPoint(hitSound[Random.Range(0, hitSound.Count)], transform.position);
-        transform.DOPunchScale(Vector3.one * .3f, 0.1f, 0, 0);
         var Player = collider.gameObject.GetComponent<PlayerHit>();
         if (Player)
         {
+            if(!IsPositive)
+            {
             Player.Hit();
+            }
+            else
+            {
+                Player.Gain();
+                ScoreManager.Instance.Score += 10;
+            }
             ScoreManager.Instance.Score++;
         }
     }
